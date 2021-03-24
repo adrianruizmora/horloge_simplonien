8ae625c6436a18bb80932ba01e099a3ecb0a8072
Adrian <adrianruizmora@hotmail.com>
2021-03-24 12:29:48 +0100

    set the html right

--- horloge_simplonien/api.py
+++ horloge_simplonien/api.py
@@ -7,9 +7,15 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route("/")
-def affichage_string():
+@app.route("/clock")
+def index():
     return clock()
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
